481b805b2c1bd9698ca3505961a695e5d9e67306
Calebe Birer <90878309+CalebeMito@users.noreply.github.com>
2023-12-06 11:39:47 -0300

    Create desconto.js

---
+++ desconto.js
@@ -0,0 +1,16 @@
+let valor = 1
+let qntd = 150
+let desconto1 = 0.05
+let desconto2 = 0.1
+let deconto3 = 0.2
+let total = valor*qntd
+
+if(qntd>500){
+console.log("valor total é:",total-(totaldesconto3))
+}else if(qntd>250){
+console.log("valor total é:",total-(totaldesconto2))
+}else if(qntd>100){
+console.log("valor total é:",total-(total*desconto1))
+}else{
+console.log("valor total é:",total)
+}
