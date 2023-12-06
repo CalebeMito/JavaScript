c0d42f50268b7e51f6527adb92355d1e67ce34d1
Calebe Birer <90878309+CalebeMito@users.noreply.github.com>
2023-12-06 11:41:35 -0300

    Create For.js

---
+++ For.js
@@ -0,0 +1,11 @@
+let i = 0
+let valorrepeticao = 10
+
+console.log('qunatos vezez vai repetir ?')
+
+valorrepeticao = valorrepeticao
+
+for (i= 0; i<=valorrepeticao; i++)
+{
+console.log('good moorning')
+}
