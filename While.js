a462427c3b1c82afc1ed13f1ec6b6a2538fab4a9
Calebe Birer <90878309+CalebeMito@users.noreply.github.com>
2023-12-06 11:41:11 -0300

    Create While.js

---
+++ While.js
@@ -0,0 +1,17 @@
+let numerosaida = 100
+let total = 9
+let sobrdiv = 0
+
+console.log ('digite um numero')
+
+while ( numerosaida <= 999){
+      if (numerosaida % 2 )
+    {   
+        console.log('esse numero e PAR', numerosaida)
+
+    }
+    else {
+      console.log('esse numero e impar', numerosaida)
+      numerosaida = numerosaida + 1       
+    }
+}
